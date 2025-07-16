4a0da6adbefbb49cf9c5feda3b09e09bebcc6e80
suji100005 <waterproofji@gmail.com>
2025-07-16 13:56:05 +0900

    Add files via upload

---
+++ student_data.js
@@ -0,0 +1,6 @@
+const studentData = {
+  "장유안": { category: "BUD" },
+  "박유진": { category: "SEED" },
+  "김재이(Jaye)": { category: "BUD" },
+  "김재이": { category: "SEED" }
+};
---
+++ word_list.js
@@ -0,0 +1,10 @@
+const wordList = {
+  "SEED": [
+    { "id": 211, "word": "travel", "meaning": "여행하다" },
+    { "id": 212, "word": "cook", "meaning": "요리하다" }
+  ],
+  "BUD": [
+    { "id": 691, "word": "candy", "meaning": "사탕" },
+    { "id": 692, "word": "classroom", "meaning": "교실" }
+  ]
+};
